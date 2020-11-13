257d1436781ec1ecc18c40281fce681041c412e7
JetechNY <sunjetliu@gmail.com>
2020-11-13 00:26:47 -0500

    added images

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ mpg: "34 EPA-EST HWY MPG",
 seats: 5,
 description: "YOUR ROAD AWAITS. Revel in every bend of the road with the CX-3’s responsive Skyactiv®-G 2.0-liter engine. No matter where that road leads you, the available predictive i-Activ AWD® will help get you there with confidence. The latest CX-3 is every bit as adventurous as you are. And that's saying something.",
 favorite: false,
-image: "https://www.mazdausa.com/siteassets/vehicles/2020/cx-3/trims/sport/2020-cx3-sport-machine-gray.png",
+image: "https://www.mazdausa.com/siteassets/vehicles/2020/cx-3/trims/sport/2020-mazda-cx-3-trims-sport.jpg",
 make: Make.first
 },
 {name: "CX-30",
@@ -23,7 +23,7 @@ mpg: "EPA-estimated mileage city/highway (mpg): 25/33",
 seats: 5,
 description: "MORE POWER. MORE CONFIDENCE. TURBOCHARGED ENGINE & i-ACTIV AWD. No matter where your path leads, the Mazda CX-30 2.5 Turbo2 is made to help you follow it. The Skyactiv®-G 2.5 Turbo engine2 delivers up to a responsive 250 hp3 and 320 lb-ft of torque.3 Paired with i-Activ AWD®, this CX-30 offers dynamic performance that makes every moment worth savoring.",
 favorite: false,
-image: "https://www.mazdausa.com/siteassets/vehicles/2021/cx-30/carousel/angles/2021-mazda-cx-30-packages-peferred-optimized.png",
+image: "https://www.mazdausa.com/siteassets/vehicles/2021/cx-30/trims/premium/2021-cx-30-premium-deepcrystalblue.png",
 make: Make.first
 },
 {name: "CX-5",
@@ -36,7 +36,7 @@ mpg: "EPA-estimated mileage city/highway (mpg): 25/315",
 seats: 5,
 description: "EFFORTLESS PERFORMANCE AND CONTROL. The Mazda CX-5 offers an available 2.5 Turbo engine that delivers up to 250 hp2 and 320 lb-ft of torque.2 Paired with i-Activ AWD®, this dynamic CUV offers impressive power and handling you can feel from the moment you step on the accelerator.",
 favorite: false,
-image: "https://www.mazdausa.com/siteassets/vehicles/2021/cx-5/trims/signature/2021-Mazda-CX-5-Trims-Signature.jpg",
+image: "https://www.mazdausa.com/siteassets/vehicles/2021/cx-5/trims/signature/2021-cx-5-sig-eternalblue.png",
 make: Make.first
 },
 {name: "CX-5 SKYACTIV-D",
@@ -62,7 +62,7 @@ mpg: "26 EPA-EST HWY MPG",
 seats: 7,
 description: "SOPHISTICATION AND PERFORMANCE IN PERFECT HARMONY. STYLE MEETS PERFORMANCE. While the average 7-seat CUV is out of touch with the road, the CX-9 is on a first-name basis with it.3 The Skyactiv®-G 2.5 Dynamic Pressure Turbo engine provides effortless acceleration when and where you need it most. Available i-Activ AWD® helps predict traction loss. G-Vectoring Control Plus helps improve cornering stability with subtle adjustments to engine torque and brake pressure. You're well-equipped to handle every road you take.",
 favorite: false,
-image: "https://www.mazdausa.com/siteassets/vehicles/2021/cx-9/trims/signature/carousel/2020-mazda-cx-9-packages-select-optimized-signaturev3.png",
+image: "https://www.mazdausa.com/siteassets/vehicles/2021/cx-9/trims/signature/2021-cx-9-awd-signature-snowflake-white-pearl-mica.png",
 make: Make.first
 },
 {name: "MAZDA3 SEDAN",
@@ -88,7 +88,7 @@ mpg: "31 EPA-EST HWY MPG",
 seats: 5,
 description: "SEDAN'S PROVOCATIVE PARTNER. A HATCHBACKER'S GUIDE TO TURBO. Just beyond the velvet ropes of convention, the Mazda3 2.5 Turbo Hatchback awaits with the motor running. With up to 250 hp2 and up to 320 lb-ft of torque,2 coupled with our i-Activ AWD®, this hatchback responds instantly and smoothly to every urge and impulse.",
 favorite: false,
-image: "https://www.mazdausa.com/siteassets/vehicles/2021/mazda3-hatchback/trims/carousel/2021-Mazda-3-Hatchback-2.5-Turbo-Package.png",
+image: "https://www.mazdausa.com/siteassets/vehicles/2021/mazda3-hatchback/trims/34-profiles/premium-plus/2021-mazda3-hatchback-turbo-premium-plus-jet-black.png",
 make: Make.first
 },
 {name: "MAZDA6",
@@ -101,7 +101,7 @@ mpg: "31 EPA-EST HWY MPG",
 seats: 5,
 description: "A POWERFUL WAY TO MAKE A SIGNATURE STATEMENT. EXHILARATION IS JUST A STEP AWAY. When you ask, it answers. Emphatically so. The Mazda6 can be equipped with our available Dynamic Pressure Turbo Skyactiv®-G 2.5T engine. Delivering up to 2509 hp and 310 lb-ft of torque, it's engineered to be ultra-responsive at low rpm, with power to spare when you're already off and running. It's an effortless brand of power that's sure to elevate your mood. Along with your heart rate.",
 favorite: false,
-image: "https://www.mazdausa.com/siteassets/vehicles/2021/mazda6/trim-package-carousel/2021-Mazda-6-Trims-Signature.png",
+image: "https://www.mazdausa.com/siteassets/vehicles/2021/mazda6/trims/signature/2021-mazda6-signature-machine-gray.png",
 make: Make.first
 },
 {name: "MAZDA MX-5 MIATA",
@@ -127,9 +127,10 @@ mpg: "35 EPA-EST HWY MPG",
 seats: 2,
 description: "GIVE IN TO THE TEMPTATION. ICONIC DNA. PROGRESSIVE STYLE. An adventurous evolution of the iconic MX-5 Miata roadster, the MX-5 Miata RF isn't just a hardtop, and it isn't just a convertible. In about 13 seconds, its retractable roof slips away, retaining the poised fastback profile with sinuous lines that always seem to be in motion. Polished, charismatic, athletic.",
 favorite: false,
-image: "https://www.mazdausa.com/siteassets/vehicles/2020/mx-5-rf/trims/gt/34-jellies/2020-Mazda-MX-5-Miata-RF-Grand-Touring.png",
+image: "https://www.mazdausa.com/siteassets/vehicles/2020/mx-5-rf/trims/gt/34-jellies/2020-mazda-mx-5-miata-rf-grand-touring.png",
 make: Make.first
 }]
+
 
 
 mazdas.each do |model|
@@ -137,7 +138,7 @@ mazdas.each do |model|
 end
 
 
-20.times do
+30.times do
     Review.create(review: Faker::Marketing.buzzwords, model: Model.all.sample)
 end
 
